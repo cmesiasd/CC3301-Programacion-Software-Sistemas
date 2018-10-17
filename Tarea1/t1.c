935649c657c461a3b5497edf6addc1e03bb91ba3
Cristóbal Mesías <32551243+cmesiasd@users.noreply.github.com>
2018-10-17 19:36:03 -0400

    Parte 2

--- Tarea1/t1.c
+++ Tarea1/t1.c
@@ -5,19 +5,56 @@
 
 uint borrar_bits(uint x, uint pat, int len){
 	int i;
+	uint mask,mask2,resto;
+    mask = ((1<<len)-1);
 	for(i=0; i<=32-len ;i++){
-        uint mask;
-        uint mask2;
-        uint resto;
-        mask = ((1<<len)-1);
         mask2 = mask<<(32-len-i);
         resto = (x & mask2)>>(32-len-i);
         if (resto == pat){
             x = x & ~mask2;
         }
-        else{
-            mask >>= 1;
-        }
     }
     return x;
 }
+
+char *reemplazo(char *s, char c, char *pal){
+    int len_s,len_pal,count_c,len_malloc;
+    len_s = 0;
+    len_pal = 0;
+    count_c = 0;
+    len_malloc = 0;
+    while(*s){
+        if (c == *s){
+            count_c++;
+        }
+        len_s++;
+        s++;
+    }
+    while(*pal){
+        len_pal++;
+        pal++;
+    }
+
+    len_malloc = len_s + count_c * (len_pal-1);
+    char *res = (char*)malloc(len_malloc);
+    s -= len_s;
+    pal -= len_pal;
+    while(*s){
+        if (c == *s){
+            while(*pal){
+                *res = *pal;
+                res++;
+                pal++;
+            }
+            pal -= len_pal;
+        }
+        else{
+            *res = *s;
+            res++;
+        }
+        s++;
+    }
+    res -= len_malloc;
+    return res;
+}
+
